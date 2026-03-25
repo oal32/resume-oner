149c69e940a66c3e1d135744709ead28103a6dc4
Ian Armstrong <iarmstrong@nc.rr.com>
2026-03-25 18:02:27 -0400

    add ip logging

--- server/main.go
+++ server/main.go
@@ -5,9 +5,21 @@ import (
 	"net/http"
 )
 
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ipAddress := r.Header.Get("X-Forwarded-For")
+
+		if ipAddress == "" {
+			ipAddress = r.RemoteAddr
+		}
+		log.Printf("Request: %s %s. From address %s", r.Method, r.URL.Path, ipAddress)
+		next.ServeHTTP(w, r)
+	})
+}
 func main() {
+
 	fs := http.FileServer(http.Dir("../resume-client/dist"))
-	http.Handle("/", fs)
+	http.Handle("/", LoggingMiddleware(fs))
 
 	log.Println("Listening on :8080...")
 	err := http.ListenAndServe(":8080", nil)
